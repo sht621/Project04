791ff6d8e770cf58f92fc41cd94aa7bbdf45d15c
Takuto0106 <al22011@shibaura-it.ac.jp>
2024-07-09 19:21:05 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/PaymentService.java
@@ -0,0 +1,68 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.mapper.PaymentMapper;
+import com.example.demo.model.PaymentModel;
+
+@Service
+@Transactional
+public class PaymentService {
+
+    private final PaymentMapper mapper;
+    
+    @Autowired
+    public PaymentService(PaymentMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public List<PaymentModel> findAll(int userId) {
+        return mapper.findAll(userId);
+    }
+
+    public PaymentModel insertPayment(PaymentModel payment) {
+    	int max = 0;
+    	if(mapper.selectMaxIdFromPayment() != null) {
+			max = mapper.selectMaxIdFromPayment().intValue();
+		}
+		else {
+			max = 0;
+		}
+		int startId = max + 1;	
+    	payment.setId(startId);
+    	mapper.inputPayment(payment);
+        return payment;
+    }
+
+    @Transactional
+    public int paymentUpdate(int income, int spend, int day, String itemId, int id, int userId) {
+        return mapper.updatePayment(income, spend, day, itemId, id, userId);
+    }
+    
+    public PaymentModel findById(int id) {
+        return mapper.findById(id);  // 引数を PaymentModel から int に変更
+    }
+    
+    public PaymentModel findByIdAndUserId(int id, int userId) {
+        return mapper.findByIdAndUserId(id, userId);
+    }
+    
+    @Transactional
+    public void deleteAndRearrange(int id, int userId) {
+    	 
+    	try {
+    	        // データの削除
+    	        mapper.deletePaymentById(id, userId);
+    	        // IDの更新
+    	        mapper.decrementIdsAfter(id);
+    	    } catch (Exception e) {
+    	        // エラーハンドリング
+    	        throw new RuntimeException("Error occurred while deleting and rearranging payments", e);
+    	    }
+    	
+    }
+}
